4cdb751022c67edd156bfa960192aa7fa1dcb30c
Valery1971 <podval1971@gmail.com>
2023-03-12 23:28:09 +0300

    Add Task 15

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Task 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int SecondDigit(int num)
 {
     int second = (num / 10) % 10;
@@ -12,7 +12,42 @@ if (number > 99 && number < 1000)
     Console.WriteLine($"The second digit in the number is {SecondDigit(number)}");
 else
     Console.WriteLine("A number is NOT three-digit");
+*/
 
 //Task 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+int ThirdDigit(int num)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
 
-void 
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100) 
+    Console.WriteLine("A number has NO Third digit");
+else
+    Console.WriteLine($"The third in the number is {ThirdDigit(number)}");
+*/
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+string WeekDay(int day)
+{
+    if(day == 6 || day == 7)
+        return "This is a Weekend!";
+    else
+        return "This is a working day";
+}
+
+Console.Write("Enter the day of the week: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 0 && number < 8)
+    Console.WriteLine(WeekDay(number));
+else
+    Console.WriteLine("There are only seven days in a week, enter a value from 1 to 7");
