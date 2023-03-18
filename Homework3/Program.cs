0dc0085a083c7a4590b5f8dd4c5e451fb548efe2
Valery1971 <podval1971@gmail.com>
2023-03-18 11:56:15 +0300

    Add task23

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -77,3 +77,8 @@ Console.WriteLine($"Distance = {distance}");
 
 // Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for(int i=1; i<=number; i++)
+    Console.Write($"{Math.Pow(i, 3)} ");
