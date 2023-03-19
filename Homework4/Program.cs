95e684b9d7cdb80c9b66bf01da25179cc5932a42
Valery1971 <podval1971@gmail.com>
2023-03-19 10:43:49 +0300

    Добавлено решение зад.25

---
+++ Homework4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числа: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+double sum = 1;
+for(int i=1; i<=degree; i++)
+    sum = sum * number;
+    
+Console.WriteLine($"Число {number} в степени {degree} равно {sum}");
+
