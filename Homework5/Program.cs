16622df6f26510e074904b5bcc786ae544b30bd3
Valery1971 <podval1971@gmail.com>
2023-03-21 21:59:58 +0300

    Добавлена задача34

---
+++ Homework5/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] FillArrayRandom(int size)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayRandom(size);
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Исходный массив: [{string.Join(" ", array)}]");
+Console.WriteLine($"Количество четных элементов: {count}");
