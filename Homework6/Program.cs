15c79d545e825232426a0fb669ea6f3884869615
Valery1971 <podval1971@gmail.com>
2023-03-24 21:58:24 +0300

    Добавлена проверка параллельности

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -34,6 +34,12 @@ double k2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите значение b2: ");
 double b2 = Convert.ToInt32(Console.ReadLine());
 
+if(k1 == k2)
+    {
+     Console.WriteLine("Прямые параллельны");
+     return;
+    }
+
 double x = (b2 - b1) / (k1 - k2);
 double y = k1 * x + b1;
 
