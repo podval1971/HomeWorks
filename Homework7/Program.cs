b94eddaa78c3d352927cfc2ded32f7e98451f770
Valery1971 <podval1971@gmail.com>
2023-03-25 16:00:26 +0300

    Добавлена задача 50

--- Homework7/Program.cs
+++ Homework7/Program.cs
@@ -1,19 +1,54 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-double[,] FillMatrixRandom(int row, int column)
+// double[,] FillMatrixRandom(int row, int column)
+// {
+//     double[,] arr = new double[row,column];
+//     Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i,j] = rnd.Next(0, 10) + Math.Round(rnd.NextDouble(), 2);
+//         }
+//     }
+//     return arr;
+// }
+
+// void PrintMatrix(double[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// double[,] matrix = FillMatrixRandom(row,column);
+// PrintMatrix(matrix);
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+int[,] FillMatrixRandom(int row, int column)
 {
-    double[,] arr = new double[row,column];
+    int[,] arr = new int[row,column];
     Random rnd = new Random(); // создаем объект класса (переменную типа Random)
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            arr[i,j] = rnd.Next(0, 10) + Math.Round(rnd.NextDouble(), 2);
+            arr[i,j] = rnd.Next(0, 10);
         }
     }
     return arr;
 }
 
-void PrintMatrix(double[,] array)
+void PrintMatrix(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -29,7 +64,14 @@ Console.Write("Введите количество строк массива: ")
 int row = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов массива: ");
 int column = Convert.ToInt32(Console.ReadLine());
-double[,] matrix = FillMatrixRandom(row,column);
+int[,] matrix = FillMatrixRandom(row,column);
 PrintMatrix(matrix);
 
-// 
+Console.Write("Введите строку элемента: ");
+int i0 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите столбец элемента: ");
+int j0 = Convert.ToInt32(Console.ReadLine());
+if(i0 < row && j0 < column)
+    Console.Write($"Значение элемента [{i0},{j0}] равно {matrix[i0,j0]}");
+else
+    Console.Write($"Элемента [{i0},{j0}] в матрице нет");
