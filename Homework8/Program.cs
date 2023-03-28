e645990e5fac51f791526a6be8613905930d5caf
Valery1971 <podval1971@gmail.com>
2023-03-28 19:03:25 +0300

    Добавлена задача 56

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -25,24 +25,62 @@ void PrintMatrix(int[,] array)
     }
 }
 
-void RowStream(int[,] arr)
+// void RowStream(int[,] arr)
+// {
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int k = 0; k < arr.GetLength(1)-1; k++)
+//         {
+//             int max = arr[i,k];
+//             for (int j = k+1; j < arr.GetLength(1); j++)
+//             {
+//                 if(arr[i,j] > max)
+//                 {
+//                     max = arr[i,j];
+//                     arr[i,j] = arr[i,k];
+//                     arr[i,k] = max;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixRandom(row,column);
+// PrintMatrix(matrix);
+// RowStream(matrix);
+// Console.WriteLine();
+// PrintMatrix(matrix);
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+void SumMinRow(int[,] arr)
 {
+    int k = 1;
+    int[] array = new int[arr.GetLength(0)];
     for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int k = 0; k < arr.GetLength(1)-1; k++)
+        int sum = 0;
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            int max = arr[i,k];
-            for (int j = k+1; j < arr.GetLength(1); j++)
-            {
-                if(arr[i,j] > max)
-                {
-                    max = arr[i,j];
-                    arr[i,j] = arr[i,k];
-                    arr[i,k] = max;
-                }
-            }
+            sum += arr[i,j];
+        }
+        array[i] = sum;
+        Console.Write($"{array[i]} ");
+    }
+    int temp = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if(array[i] < temp)
+        {
+          k = i+1;
+          temp = array[i];
         }
     }
+    Console.WriteLine();
+    Console.WriteLine($"Наименьшая сумма элементов у {k} строки");
 }
 
 Console.Write("Введите количество строк массива: ");
@@ -51,6 +89,4 @@ Console.Write("Введите количество столбцов массив
 int column = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = FillMatrixRandom(row,column);
 PrintMatrix(matrix);
-RowStream(matrix);
-Console.WriteLine();
-PrintMatrix(matrix);
+SumMinRow(matrix);
