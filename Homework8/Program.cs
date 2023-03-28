e7168ed9a0a513b9497a7ae4b2259d391e8c4dcd
Valery1971 <podval1971@gmail.com>
2023-03-28 19:23:19 +0300

    Добавлена задача 60

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -1,29 +1,29 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-int[,] FillMatrixRandom(int row, int column)
-{
-    int[,] arr = new int[row,column];
-    Random rnd = new Random(); // создаем объект класса (переменную типа Random)
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            arr[i,j] = rnd.Next(0, 10);
-        }
-    }
-    return arr;
-}
+// int[,] FillMatrixRandom(int row, int column)
+// {
+//     int[,] arr = new int[row,column];
+//     Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i,j] = rnd.Next(0, 10);
+//         }
+//     }
+//     return arr;
+// }
 
-void PrintMatrix(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i,j]} \t");
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
 // void RowStream(int[,] arr)
 // {
@@ -56,37 +56,80 @@ void PrintMatrix(int[,] array)
 // PrintMatrix(matrix);
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-void SumMinRow(int[,] arr)
+// void SumMinRow(int[,] arr)
+// {
+//     int k = 1;
+//     int[] array = new int[arr.GetLength(0)];
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             sum += arr[i,j];
+//         }
+//         array[i] = sum;
+//         Console.Write($"{array[i]} ");
+//     }
+//     int temp = array[0];
+//     for (int i = 1; i < array.Length; i++)
+//     {
+//         if(array[i] < temp)
+//         {
+//           k = i+1;
+//           temp = array[i];
+//         }
+//     }
+//     Console.WriteLine();
+//     Console.WriteLine($"Наименьшая сумма элементов у {k} строки");
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+// int[,] matrix = FillMatrixRandom(row,column);
+// PrintMatrix(matrix);
+// SumMinRow(matrix);
+
+// Задача 60. Сформируйте трёхмерный массив. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int[,,] FillTreeDimRandom(int x, int y, int z)
 {
-    int k = 1;
-    int[] array = new int[arr.GetLength(0)];
+    int[,,] arr = new int[x,y,z];
+    Random rnd = new Random(); // создаем объект класса (переменную типа Random)
     for (int i = 0; i < arr.GetLength(0); i++)
     {
-        int sum = 0;
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            sum += arr[i,j];
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+               arr[i,j,k] = rnd.Next(0, 10);
+            }
         }
-        array[i] = sum;
-        Console.Write($"{array[i]} ");
     }
-    int temp = array[0];
-    for (int i = 1; i < array.Length; i++)
+    return arr;
+}
+
+void PrintTreeDim(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        if(array[i] < temp)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-          k = i+1;
-          temp = array[i];
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i,j,k]}({i},{j},{k})  ");
+            }
         }
     }
-    Console.WriteLine();
-    Console.WriteLine($"Наименьшая сумма элементов у {k} строки");
 }
 
 Console.Write("Введите количество строк массива: ");
-int row = Convert.ToInt32(Console.ReadLine());
+int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов массива: ");
-int column = Convert.ToInt32(Console.ReadLine());
-int[,] matrix = FillMatrixRandom(row,column);
-PrintMatrix(matrix);
-SumMinRow(matrix);
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите глубину трехмерного массива: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] matrix = FillTreeDimRandom(x,y,z);
+PrintTreeDim(matrix);
+
+// 
