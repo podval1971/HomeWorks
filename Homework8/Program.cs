208e1b1f2241ef4e9c06ef6ba567d6b3beeb5e4f
Valery1971 <podval1971@gmail.com>
2023-03-28 20:37:04 +0300

    Добавлена задача 62

--- Homework8/Program.cs
+++ Homework8/Program.cs
@@ -56,6 +56,32 @@
 // PrintMatrix(matrix);
 
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// int[,] FillMatrixRandom(int row, int column)
+// {
+//     int[,] arr = new int[row,column];
+//     Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i,j] = rnd.Next(0, 10);
+//         }
+//     }
+//     return arr;
+// }
+
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i,j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
 // void SumMinRow(int[,] arr)
 // {
 //     int k = 1;
@@ -92,44 +118,128 @@
 // SumMinRow(matrix);
 
 // Задача 60. Сформируйте трёхмерный массив. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-int[,,] FillTreeDimRandom(int x, int y, int z)
+// int[,,] FillTreeDimRandom(int x, int y, int z)
+// {
+//     int[,,] arr = new int[x,y,z];
+//     Random rnd = new Random(); // создаем объект класса (переменную типа Random)
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < arr.GetLength(2); k++)
+//             {
+//                arr[i,j,k] = rnd.Next(0, 10);
+//             }
+//         }
+//     }
+//     return arr;
+// }
+
+// void PrintTreeDim(int[,,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(2); k++)
+//             {
+//                 Console.Write($"{array[i,j,k]}({i},{j},{k})  ");
+//             }
+//         }
+//     }
+// }
+
+// Console.Write("Введите количество строк массива: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов массива: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите глубину трехмерного массива: ");
+// int z = Convert.ToInt32(Console.ReadLine());
+// int[,,] matrix = FillTreeDimRandom(x,y,z);
+// PrintTreeDim(matrix);
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+int[,] FillMatrixSpiral(int row, int column)
 {
-    int[,,] arr = new int[x,y,z];
-    Random rnd = new Random(); // создаем объект класса (переменную типа Random)
-    for (int i = 0; i < arr.GetLength(0); i++)
+    int[,] arr = new int[row,column];
+    int index = 1;
+    for (int j = 0; j < arr.GetLength(1); j++) // заполняем периметр, движемся вправо
     {
-        for (int j = 0; j < arr.GetLength(1); j++)
+        arr[0,j] = index;
+        index++;
+    }
+    for (int i = 1; i < arr.GetLength(0); i++) // движемся вниз
+    {
+        arr[i,arr.GetLength(1)-1] = index;
+        index++;
+    }
+    for (int j = arr.GetLength(1)-2; j >= 0; j--) // движемся влево
+    {
+        arr[arr.GetLength(0)-1,j] = index;
+        index++;
+    }
+    for (int i = arr.GetLength(0)-2; i > 0; i--) // движемся вверх
+    {
+        arr[i,0] = index;
+        index++;
+    }
+
+    int x = 1; // заполняем серединку, координаты первой ячейки
+    int y = 1;
+    while (index < row * column)
+    {
+        while (arr[x,y+1] == 0) // движемся вправо
         {
-            for (int k = 0; k < arr.GetLength(2); k++)
+            arr[x,y] = index;
+            index++;
+            y++;
+        }
+        while (arr[x+1,y] == 0) // движемся вниз
+        {
+            arr[x,y] = index;
+            index++;
+            x++;
+        }
+        while (arr[x,y-1] == 0) // движемся влево
+        {
+            arr[x,y] = index;
+            index++;
+            y--;
+        }
+        while (arr[x-1,y] == 0) // движемся вверх
+        {
+            arr[x,y] = index;
+            index++;
+            x--;
+        }
+        for (int i = 0; i < arr.GetLength(0); i++) // заполняем самый центр
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
             {
-               arr[i,j,k] = rnd.Next(0, 10);
+                if(arr[i,j] ==0) arr[i,j] = index;
             }
         }
     }
     return arr;
 }
 
-void PrintTreeDim(int[,,] array)
+void PrintMatrix(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int k = 0; k < array.GetLength(2); k++)
-            {
-                Console.Write($"{array[i,j,k]}({i},{j},{k})  ");
-            }
+            Console.Write($"{array[i,j]} \t");
         }
+        Console.WriteLine();
     }
 }
 
 Console.Write("Введите количество строк массива: ");
-int x = Convert.ToInt32(Console.ReadLine());
+int row = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов массива: ");
-int y = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите глубину трехмерного массива: ");
-int z = Convert.ToInt32(Console.ReadLine());
-int[,,] matrix = FillTreeDimRandom(x,y,z);
-PrintTreeDim(matrix);
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrixSpiral(row,column);
+PrintMatrix(matrix);
 
 // 
